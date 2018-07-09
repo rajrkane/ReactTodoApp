71c0634c1bf7d09e77a8a50b5697f13bd8ae96e0
rrkane <raj.r.kane@protonmail.com>
2018-07-09 11:27:31 -0700

    Display dummyData

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,4 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-ReactDOM.render(<p>React lives!</p>,
+
+let dummyData = [
+  "Buy groceries",
+  "Call mom",
+  "Clean room"
+];
+
+//An individual todo item
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  };
+  render() {
+    return(
+      <li>
+        <button type="button">X</button>
+        {this.props.task}
+      </li>
+    );
+  };
+};
+
+//The box around the assortment of Todos in our picture
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  };
+  render() {
+    return(
+      <ul>
+        {dummyData.map((item) => <Todo task={item}/>)}
+      </ul>
+    );
+  };
+};
+
+//A row at the top with an input field and submit button
+class InputLine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  };
+  render() {
+    return(
+      null
+    );
+  };
+};
+
+//A box around the whole thing
+class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  };
+  render() {
+    return(
+      null
+    );
+  };
+};
+
+
+ReactDOM.render(<TodoList />,
    document.getElementById('root'));
